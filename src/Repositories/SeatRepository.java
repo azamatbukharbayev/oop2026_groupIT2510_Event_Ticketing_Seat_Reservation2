63d025dc513d6795c5dc01febadef47611d3a3ee
azamatbukharbayev <azamatbukharbaev@gmail.com>
2026-02-06 12:00:17 +0500

    Azamat Bukharbayev - minor changes

--- src/Repositories/SeatRepository.java
+++ src/Repositories/SeatRepository.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import java.util.UUID;
 import Entities.Seat;
 
-public interface SeatRepository extends Repository<Seat, ID> {
+public interface SeatRepository extends Repository<Seat, UUID> {
 
     List<Seat> findByEventId(UUID eventId);
 }
